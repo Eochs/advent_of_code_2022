05ead27751823ae0101f8af52d1f86420f81213c
Spencer Ochs <slochastic@gmail.com>
2022-12-10 10:24:18 -0800

    day 1 part 2

--- aoc_rust/src/bin/day1.rs
+++ aoc_rust/src/bin/day1.rs
@@ -1,13 +1,16 @@
 fn main() {
     let input: &str = include_str!("./input.txt");
     let lines = input.split("\n\n");
-    let max_val = lines
+    let mut lines_parsed: Vec<u32> = lines
         .map(|line| {
             line.split("\n")
                 .flat_map(|num| num.parse::<u32>())
                 .sum::<u32>()
         })
-        .max();
+        .collect();
 
-    println!("{:?}", max_val)
+    lines_parsed.sort_by(|a, b| b.cmp(a));
+
+    println!("Part 1: {:?}", lines_parsed[0]);
+    println!("Part 2: {:?}", lines_parsed.iter().take(3).sum::<u32>())
 }
